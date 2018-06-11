ca1f00420a431eba9f41c467e2a11042dc4f6f1e
Jimmy Starling <jimmystarlingdev@gmail.com>
2018-06-10 23:33:10 -0300

    Added author comments

--- app/src/main/java/com/reactivemap/jimmystarling/reactivemapupdate/models/MessageEvent.kt
+++ app/src/main/java/com/reactivemap/jimmystarling/reactivemapupdate/models/MessageEvent.kt
@@ -1,3 +1,3 @@
 package com.reactivemap.jimmystarling.reactivemapupdate.models
-
+// Simple [data class] Created by @Author: Jimmy Starling. 
 data class MessageEvent(val latitude: Double, val longitude: Double)
